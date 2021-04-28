926db1fa412661948961048ddea25dbbd58a3324
Катя <kateru68@gmail.com>
2021-04-28 22:31:49 +0300

    исправила рекламу марса

--- изображение марса.py
+++ изображение марса.py
@@ -11,7 +11,7 @@ def index():
     return "И на Марсе будут яблони цвести!"
 
 
-@app.route('/image_sample')
+@app.route('/image_mars')
 def image():
     return f'''<h1>Жди нас, Марс!</h1><img src="{url_for('static', filename='Mars.jpg')}"\
     alt="здесь должна была быть картинка, но не нашлась"><p>Вот она, красная планета</p>'''
